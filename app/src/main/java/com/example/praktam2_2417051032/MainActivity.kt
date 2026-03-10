2cac5216afa0a293038d57def13c690318ed7286
Talitha Roihanah Salsabila <talithas337@gmail.com>
2026-03-11 00:41:45 +0700

    Tugas LKP 3

--- app/src/main/java/com/example/praktam2_2417051032/MainActivity.kt
+++ app/src/main/java/com/example/praktam2_2417051032/MainActivity.kt
@@ -10,15 +10,15 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.praktam2_2417051032.ui.theme.PrakTAM2_2417051032Theme
 
 class MainActivity : ComponentActivity() {
@@ -47,8 +47,21 @@ fun BookList(modifier: Modifier = Modifier) {
     val books = BookSource.dummyBook
 
     LazyColumn(
-        modifier = modifier.padding(16.dp)
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
     ) {
+
+        item {
+            Text(
+                text = "Rekomendasi Novel",
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold
+            )
+
+            Spacer(modifier = Modifier.height(20.dp))
+        }
+
         items(books) { book ->
             BookItem(book)
         }
@@ -61,7 +74,7 @@ fun BookItem(book: Book) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(bottom = 16.dp)
+            .padding(bottom = 20.dp)
     ) {
 
         Column(
@@ -73,15 +86,38 @@ fun BookItem(book: Book) {
                 contentDescription = book.nama,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(180.dp),
+                    .height(200.dp),
                 contentScale = ContentScale.Crop
             )
 
-            Spacer(modifier = Modifier.height(8.dp))
+            Spacer(modifier = Modifier.height(10.dp))
 
-            Text(text = "Judul Novel : ${book.nama}")
-            Text(text = "Pencipta : ${book.pencipta}")
-            Text(text = "Sinopsis : ${book.deskripsi}")
+            Text(
+                text = book.nama,
+                fontWeight = FontWeight.Bold,
+                fontSize = 20.sp
+            )
+
+            Text(
+                text = "Penulis : ${book.pencipta}",
+                fontSize = 14.sp
+            )
+
+            Spacer(modifier = Modifier.height(6.dp))
+
+            Text(
+                text = book.deskripsi,
+                fontSize = 14.sp
+            )
+
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Button(
+                onClick = { },
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Baca Sekarang")
+            }
         }
     }
 }
