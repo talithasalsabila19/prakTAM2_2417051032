c275e1a84a689a52180917a272f572fd8cf66ddc
Talitha Roihanah Salsabila <talithas337@gmail.com>
2026-03-16 17:58:47 +0700

    Tugas LKP 4

--- app/src/main/java/com/example/praktam2_2417051032/MainActivity.kt
+++ app/src/main/java/com/example/praktam2_2417051032/MainActivity.kt
@@ -7,20 +7,39 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.material3.Card
 import com.example.praktam2_2417051032.ui.theme.PrakTAM2_2417051032Theme
-
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -70,7 +89,7 @@ fun BookList(modifier: Modifier = Modifier) {
 
 @Composable
 fun BookItem(book: Book) {
-
+    var isFavorite by remember { mutableStateOf(false) }
     Card(
         modifier = Modifier
             .fillMaxWidth()
@@ -79,17 +98,28 @@ fun BookItem(book: Book) {
 
         Column(
             modifier = Modifier.padding(12.dp)
-        ) {
 
-            Image(
-                painter = painterResource(id = book.gambar),
-                contentDescription = book.nama,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .height(200.dp),
-                contentScale = ContentScale.Crop
-            )
-
+        ){
+            Box {
+                Image(
+                    painter = painterResource(id = book.gambar),
+                    contentDescription = book.nama,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(200.dp),
+                    contentScale = ContentScale.Crop
+                )
+                IconButton(
+                    onClick = { isFavorite = !isFavorite },
+                    modifier = Modifier.align(Alignment.TopEnd)
+                ) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Filled.Favorite else Icons.Outlined.FavoriteBorder,
+                        contentDescription = "Favorite Icon",
+                        tint = if (isFavorite) Color.Red else Color.White
+                    )
+                }
+            }
             Spacer(modifier = Modifier.height(10.dp))
 
             Text(
@@ -118,6 +148,7 @@ fun BookItem(book: Book) {
             ) {
                 Text("Baca Sekarang")
             }
+
         }
     }
 }
